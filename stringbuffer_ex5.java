885bac2eb65e0684abf12a76f90faad891a51341
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 09:38:40 +0530

    Create stringbuffer_ex5.java

---
+++ stringbuffer_ex5.java
@@ -0,0 +1,12 @@
+package files;
+
+public class string_buffer_ex5 {
+
+	public static void main(String[] args) {
+		StringBuffer sb =new StringBuffer("hello");
+		sb.reverse();
+		System.out.println(sb);
+
+	}
+
+}
