0abb6957450fdcf3d7d35324e9bae4e83a6d2c98
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 09:36:40 +0530

    Create stringbuffer_ex1.java

---
+++ stringbuffer_ex1.java
@@ -0,0 +1,13 @@
+package files;
+
+public class String_buffer_ex {
+
+	public static void main(String[] args)
+	{
+		StringBuffer sb =new StringBuffer("hello");
+		sb.append("world");
+		System.out.println(sb);
+
+	}
+
+}
