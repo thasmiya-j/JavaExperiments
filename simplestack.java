4b48d1d159490a19cddd739c92bf8f441201a022
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 10:09:06 +0530

    Create simplestack.java

---
+++ simplestack.java
@@ -0,0 +1,19 @@
+package practice;
+
+class Stackjava
+{
+int s[]=new int[10];
+int top;
+}
+public class Stack_ {
+	public static void main(String[] args) 
+	{
+		Stackjava s1 = new Stackjava();
+		s1.push(10);
+		s1.push(9);
+		
+		
+
+	}
+
+}
