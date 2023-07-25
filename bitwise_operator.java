cb401e2fbedc6cb7e393fa3ef001cd49d0ca1c18
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 09:12:37 +0530

    Create bitwise_operator.java

---
+++ bitwise_operator.java
@@ -0,0 +1,23 @@
+
+public class bitwise_operator_ {
+
+	public static void main(String[] args) 
+	{
+		int a= 5,b = 6;
+		int c = a&b;
+		System.out.println("a&b is "+c);
+		c = a|b;
+		System.out.println("a|b is "+c);
+		c = a^b;
+		System.out.println("a&b is "+c);
+		c = ~a;
+		System.out.println("~a is "+c);
+		c = a<<1;
+		System.out.println("left shift of a  "+c);
+		c = a>>1;
+		System.out.println("right shift of a  "+c);
+		
+		
+	}
+
+}
