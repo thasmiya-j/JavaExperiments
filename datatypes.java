da4e539c29c588e1e01ade919d99b96f2dc95b30
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 09:44:53 +0530

    Create datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+package practice;
+
+
+public class First 
+{
+	public static void main(String[]args)
+	{
+	int a=10;// 32 bit
+	boolean b = false;//true or false
+	byte c = 120;//8 bits (-127 to 128)
+	short d = 2000;//16 bits(-32,768 tom 32,767)
+	long e = 11111;// 64 bit
+	float f = 50;//unlimited
+	double g = 34.9;//64 bit used for decimsl values 
+	char h = 'r';//2 byte
+	
+	System.out.println( "A:"+a+ " "+b+" "+c+"  "+d+" "+e+" "+f+" "+g+" "+h);
+	
+	}
+	
+
+}
