c95fccc1977fdf1a311f23a92a7eefe279cd1c45
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 10:01:36 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,37 @@
+//package university_lab;
+//set8
+//16
+class Test<T> {
+    // An object of type T is declared
+    T obj;
+    Test(T obj)
+    {
+    	this.obj = obj; 
+    } // constructor
+    public T getObject() 
+    { 
+    	return this.obj; 
+    	
+    }
+}
+
+public class main_ {
+
+	public static void main(String[] args)
+	{
+		
+		 // instance of Integer type
+		
+        Test<Integer> iObj = new Test<Integer>(15);
+        System.out.println(iObj.getObject());
+  
+        // instance of String type
+        
+        Test<String> sObj= new Test<String>("GeeksForGeeks");
+        System.out.println(sObj.getObject());
+
+	}
+
+}
+
+j
