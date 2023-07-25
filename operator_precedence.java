73a41c073ec9b1084d61168d15ec242921b33657
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 09:23:38 +0530

    Create operator_precedence.java

---
+++ operator_precedence.java
@@ -0,0 +1,15 @@
+
+public class operator_precedence {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a =3 ,b =9,c;
+		c= a*b-10;
+		System.out.println(c);
+		
+		
+
+	}
+
+}
