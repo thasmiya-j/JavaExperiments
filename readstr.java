daa37046a5b6ac8f8298fa3e9f6d877a2b337fb4
thasmiya-j <112604308+thasmiya-j@users.noreply.github.com>
2023-07-25 09:51:13 +0530

    Create readstr.java

---
+++ readstr.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class read_string {
+
+	public static void main(String[] args)
+	{
+		String str ;
+		System.out.println("enter the string:");
+		Scanner r =new Scanner(System.in);
+		str= r.nextLine();
+		System.out.println("you have entered the string:"+str);
+		
+	}
+
+}
